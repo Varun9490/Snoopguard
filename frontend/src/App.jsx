3a0c726e5d065beab2af3329fd1c6f30eca14eb0
nadipena varunkumar <129164065+Varun9490@users.noreply.github.com>
2024-12-21 10:28:08 +0530

    fixed a bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar.jsx";
 
-import HomePage from "./pages/HomePage";
-import SignUpPage from "./pages/SignUpPage";
-import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/signUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 
